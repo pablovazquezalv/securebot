e8b0e6ec7f7ae8938f1f976f5d5cfd9cf46ea824
Hectorfrdz <hectorflores_rdz@outlook.com>
2023-04-12 21:06:47 -0600

    paginacion y busqueda v.200

--- src/app/Componentes/Admin/administrar_usuarios/verusuarios/verusuarios.component.ts
+++ src/app/Componentes/Admin/administrar_usuarios/verusuarios/verusuarios.component.ts
@@ -6,15 +6,28 @@ import { ModalConfirmacionStatusComponent } from '../../modal-confirmacion-statu
 import { RolesModificarComponent } from '../../roles-modificar/roles-modificar.component';
 import { PageEvent } from '@angular/material/paginator';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { trigger, transition, style, animate } from '@angular/animations';
+
+export const fadeInOut = trigger('fadeInOut', [
+  transition(':enter', [
+    style({ opacity: 0 }),
+    animate('500ms ease-out', style({ opacity: 1 })),
+  ]),
+  transition(':leave', [
+    animate('500ms ease-out', style({ opacity: 0 })),
+  ]),
+]);
 
 @Component({
   selector: 'app-verusuarios',
   templateUrl: './verusuarios.component.html',
-  styleUrls: ['./verusuarios.component.css']
+  styleUrls: ['./verusuarios.component.css'],
+  animations: [fadeInOut],
 })
 export class VerusuariosComponent implements OnInit {
   users: User[] = [];
   pageSize = 5;
+  page = 0;
   desde:number = 0;
   hasta:number = 5;
   filterPost = "";
@@ -28,7 +41,7 @@ export class VerusuariosComponent implements OnInit {
 
   ngOnInit() {
     this.getUsers();
-    console.log(this.users)
+    console.log(this.filterPost)
   }
 
   getUsers() {
@@ -36,12 +49,12 @@ export class VerusuariosComponent implements OnInit {
       this.users = users.data;
   
       // Verificación adicional para ajustar pageSize y desde/hasta
-      if (this.pageSize > this.users.length) {
-        this.pageSize = this.users.length;
-      }
-      if (this.hasta > this.users.length) {
-        this.hasta = this.users.length;
-      }
+      // if (this.pageSize > this.users.length) {
+      //   this.pageSize = this.users.length;
+      // }
+      // if (this.hasta > this.users.length) {
+      //   this.hasta = this.users.length;
+      // }
 
     });
   }
@@ -67,11 +80,29 @@ export class VerusuariosComponent implements OnInit {
     this.hasta = this.desde + e.pageSize
   }
 
-  onSubmit(values: String) {
-    // console.log(values)
-    this.userService.getSearchingUsers(values).subscribe((res) => {
-      this.users = res
-    })
+  // onSubmit(values: String) {
+  //   // console.log(values)
+  //   this.userService.getSearchingUsers(values).subscribe((res) => {
+  //     this.users = res
+  //   })
+  // }
+
+  nextPage()
+  {
+    if (this.page <= this.users.length)
+      this.page += 5;
+  }
+
+  previousPage()
+  {
+    if (this.page >= 0)
+      this.page -= 5;
+  }
+
+  onSearch( filterPost: string )
+  {
+    this.page = 0;
+    this.filterPost = filterPost;
   }
 }
  
--- src/app/pipes/filter.pipe.ts
+++ src/app/pipes/filter.pipe.ts
@@ -5,24 +5,21 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterPipe implements PipeTransform {
 
-  hasta = 0
-  transform(users: any[], searchTerm: string, desde = 0, pageSize = 0): any[] {
-    if(searchTerm === "" || searchTerm.length < 3) return users
-    if (!users) {
-      return [];
-    }
-    if (!searchTerm) {
-      return users;
-    }
+  transform(users: any[], searchTerm: string, page = 0): any[] {
+    if (searchTerm.length === 0)
+      return users.slice(page,page + 5)
+
     searchTerm = searchTerm.toLowerCase();
-    this.hasta = desde + pageSize
-    return [users.filter(user => {
-      const fullName = user.name.toLowerCase() + ' ' + user.ap_paterno.toLowerCase() + ' ' + user.ap_materno.toLowerCase();
-      return fullName.includes(searchTerm)
+    const filteredUsers = users.filter(user => {
+      return user.name.toLowerCase().includes(searchTerm)
+        || user.ap_paterno.toLowerCase().includes(searchTerm)
+        || user.ap_materno.toLowerCase().includes(searchTerm)
         || user.email.toLowerCase().includes(searchTerm)
         || user.phone_number.toLowerCase().includes(searchTerm)
         || user.rol.toLowerCase().includes(searchTerm);
-    })]
+    });
+
+    return filteredUsers.slice(page,page + 5)
   }
   
 }
