7900f8011c2808618e86968ca527e2b3e06dfd10
arkadiem <jasonusotelo@gmail.com>
2023-04-21 10:49:13 -0600

    Fix Websocket

--- src/app/Services/web-socket.service.ts
+++ src/app/Services/web-socket.service.ts
@@ -5,7 +5,7 @@ import { io } from 'socket.io-client';
   providedIn: 'root'
 })
 export class WebSocketService {
-  public socket = io('https://159.203.186.210:3333');
+  public socket = io('https://securebot.ninja');
 
   constructor() { }
 
