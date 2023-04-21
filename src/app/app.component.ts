b80bca53f12b304a3de8228bc0387c72414af0f0
carlosavs04 <109000628+carlosavs04@users.noreply.github.com>
2023-04-21 13:33:53 -0600

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,13 +16,23 @@ export class AppComponent implements OnInit {
     router.events.subscribe((val) => {
       if(val instanceof NavigationEnd)
       {
-        if(val.url=="/login" || val.url=="/registrarse" || val.url=="/instrucciones" || val.url=="/code-verify" ||  val.url=="/correo-equivocado" || val.url=="/telefono-equivocado" || val.url=="/datos-carrito" || val.url=="/historial-datos"  )
+        if(val.url=="/login" || val.url=="/registrarse" || val.url=="/instrucciones" || val.url=="/code-verify" ||  val.url=="/correo-equivocado" || val.url=="/telefono-equivocado"  )
         {
           this.showHeader=false;
-          this.showSpinner=false;
+          this.showSpinner = true;
+          
         }
         else{
           this.showHeader=true;
+          this.showSpinner = false;
+          
+        }
+        if( val.url=="/datos-carrito" || val.url=="/historial-datos")
+        {
+          this.showSpinner=false;
+        }
+        else
+        {
           this.showSpinner=true;
         }
       }
