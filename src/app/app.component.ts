74926ec98b93a09b3168b9eb1e5c4b6874031ea4
carlosavs04 <109000628+carlosavs04@users.noreply.github.com>
2023-04-21 13:28:21 -0600

    spinner quitado de ciertas cosas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,17 +10,20 @@ import { WebSocketService } from './Services/web-socket.service';
 export class AppComponent implements OnInit {
   title = 'Secure-Bot';
   showHeader= true;
+  showSpinner = true;
   constructor(private router:Router, private webSocketService: WebSocketService)
    {
     router.events.subscribe((val) => {
       if(val instanceof NavigationEnd)
       {
-        if(val.url=="/login" || val.url=="/registrarse" || val.url=="/instrucciones" || val.url=="/code-verify" ||  val.url=="/correo-equivocado" || val.url=="/telefono-equivocado"  )
+        if(val.url=="/login" || val.url=="/registrarse" || val.url=="/instrucciones" || val.url=="/code-verify" ||  val.url=="/correo-equivocado" || val.url=="/telefono-equivocado" || val.url=="/datos-carrito" || val.url=="/historial-datos"  )
         {
           this.showHeader=false;
+          this.showSpinner=false;
         }
         else{
           this.showHeader=true;
+          this.showSpinner=true;
         }
       }
     })
