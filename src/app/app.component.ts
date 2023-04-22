8489a2baf9a2a239f85eba080a9ca3963d4c0d1c
Pablo Vazquez Alvarado <96793896+pablovazquezalv@users.noreply.github.com>
2023-04-22 14:55:12 -0600

    no

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,21 +18,15 @@ export class AppComponent implements OnInit {
       {
         if(val.url=="/login" || val.url=="/registrarse" || val.url=="/instrucciones" || val.url=="/code-verify" ||  val.url=="/correo-equivocado" || val.url=="/telefono-equivocado"  )
         {
-          //oculta nav 
-          this.showHeader= false;
-        }
-        else if (val.url == "/" || val.url == "**" || val.url == "/inicio" || val.url == "/conocenos" || val.url == "/users" || val.url == "/empresas" || val.url == "/empleados-empresa/:id" || val.url == "/solicitudes-empresa" || val.url == "/solicitudes-empleados" || val.url == "/empleados" || val.url == "/crear-carrito" || val.url == "/profile" || val.url == "/empleados-usuario" || val.url == "/mi-empresa" || val.url == "/crear-empresa" || val.url == "/crear-empresa" || val.url == "/afiliar-empresa" || val.url == "/autos-empresa")
-        {
-          this.showHeader=true;
+          this.showHeader=false;
           this.showSpinner = true;
+          
         }
-        
-        else if (val.url == "/datos-carrito"  || val.url == "historial-datos")
-        {
+        else{
           this.showHeader=true;
           this.showSpinner = false;
+          
         }
-        
        
       }
     })
